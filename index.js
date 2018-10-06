8f1fb5092652a22b1f1700576a7ee008e8aa5ff9
Robert Riley <rob_a_riley@yahoo.com>
2018-10-06 18:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function removeFromCart(item) {
   
   for (i=0; i<cart.length; i++){
     if (cart[i].itemName == item) {return(cart.splice(i,1));} // ends if (cart[i].itemName = name)
-    if (i === cart.length) {return "That item is not in your cart.";} // ends if
   } // ends for
+  if (i === cart.length) {return "That item is not in your cart.";} // ends if  
 } // ends function
 
 function placeOrder(cardNumber) {
